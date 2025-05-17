cc0b07ec678acebe95def16f22393299298857a6
sriharibandi <shreharii@gmail.com>
2025-05-17 06:24:46 +0530

    variable workflow

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Musk
+PERSON1=Modi
+PERSON2=Jagan
 
 echo "$PERSON1: Hey $PERSON2, how are you ?"
 echo "$PERSON2: Hello $PERSON1, I am fine. How are you doing"
