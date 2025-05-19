91268ca2b8a61ed7d2fa60398309331656565456
sriharibandi <shreharii@gmail.com>
2025-05-19 20:00:39 +0530

    variable workflow

--- arrays.sh
+++ arrays.sh
@@ -3,4 +3,10 @@
 MOVIES=("Court" "HIT3" "Pushpa2" "Thandel")
 
 
-echo "first movie name is ${MOVIES[@]}"   # @ or * or %
+echo "first movie name is ${MOVIES[0]}"    # @ or * or %
+echo "second movie name is ${MOVIES[1]}"
+echo "Third movie name is ${MOVIES[2]}"
+echo "fourth movie name is ${MOVIES[3]}"
+
+
+echo "All movies are displayed here ${MOVIES[@]}"
