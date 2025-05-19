15e7608fb3a20d551c7c98dbc16cb26d92176513
sriharibandi <shreharii@gmail.com>
2025-05-20 05:24:23 +0530

    practise

--- install-commands.sh
+++ install-commands.sh
@@ -6,6 +6,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo "ERROR: Please run the user with root access"
+    exit 1 #give other than 0 upto 127 
 else 
     echo "You are running with root access"
 fi
