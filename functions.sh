948d9cf12a26208e1167ac815ff0b4691d21bde5
sriharibandi <shreharii@gmail.com>
2025-05-20 20:56:36 +0530

    practise

---
+++ functions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR: Please run the user with root access"
+    exit 1 #give other than 0 upto 127 
+else 
+    echo "You are running with root access"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+       echo "Install $2 is ...SUCESS"
+    else
+       echo "Install $2 is ...FAILURE"
+        exit 1
+    fi}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    echo "MYSQL is not installed..... going to install"
+    dnf install mysql -y 
+     VALIDATE $? "MySql"
+else 
+    echo "MySQL is already installed and nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed..... going to install"
+    dnf install nginx sql -y 
+    VALIDATE $? "nginx"
+else 
+    echo "nginx is already installed and nothing to do"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "python3 is not installed..... going to install"
+    dnf install python3 -y 
+    if [ $? -eq 0 ]
+    VALIDATE $? "Python3"
+else 
+    echo "python3 is already installed and nothing to do"
+fi
