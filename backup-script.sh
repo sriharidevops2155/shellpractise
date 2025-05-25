041c85aecebb64c6dbe75cc583f06aa7ee12d59d
sriharibandi <shreharii@gmail.com>
2025-05-25 10:39:59 +0530

    practise5

--- backup-script.sh
+++ backup-script.sh
@@ -12,9 +12,22 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+check_root()
+{
+#check the user have root privilages or not ? 
+    if [ $USERID -ne 0 ]
+    then 
+        echo -e "$R ERROR: Please run the user with root access $N" | tee -a $LOG_FILE  
+        exit 1 #give other than 0 upto 127 
+    else 
+        echo "You are running with root access" | tee -a $LOG_FILE 
+    fi
+}
+
 check_root
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
+
 
 USAGE()
 {
@@ -37,15 +50,3 @@ if [$# -lt 2 ]
 then 
     USAGE 
 fi
-
-check_root()
-{
-#check the user have root privilages or not ? 
-    if [ $USERID -ne 0 ]
-    then 
-        echo -e "$R ERROR: Please run the user with root access $N" | tee -a $LOG_FILE  
-        exit 1 #give other than 0 upto 127 
-    else 
-        echo "You are running with root access" | tee -a $LOG_FILE 
-    fi
-}
