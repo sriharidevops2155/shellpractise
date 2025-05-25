7b885cc1449db36f36cd5b5741105e72ac8f7b4b
sriharibandi <shreharii@gmail.com>
2025-05-25 10:42:48 +0530

    practise5

--- backup-script.sh
+++ backup-script.sh
@@ -12,6 +12,18 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+#validating if installation is sucedded or not 
+VALIDATE()
+{
+    if [ $1 -eq 0 ]
+    then
+       echo -e "$2 is ...$G SUCESS $N" | tee -a $LOG_FILE 
+    else
+       echo -e " $2 is ...$R FAILURE $N "| tee -a $LOG_FILE 
+       exit 1
+    fi
+}
+
 check_root()
 {
 #check the user have root privilages or not ? 
@@ -26,24 +38,10 @@ check_root()
 
 check_root
 mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE 
-
 
 USAGE()
 {
     echo -e "$R USAGE:: $N sh 20-backup.sh <source-dir> <dest-dir> <days(optional)>"
-}
-
-#validating if installation is sucedded or not 
-VALIDATE()
-{
-    if [ $1 -eq 0 ]
-    then
-       echo -e "$2 is ...$G SUCESS $N" | tee -a $LOG_FILE 
-    else
-       echo -e " $2 is ...$R FAILURE $N "| tee -a $LOG_FILE 
-       exit 1
-    fi
 }
 
 if [$# -lt 2 ]
