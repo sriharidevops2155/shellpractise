28cdbdfbdb4075d9714e162714d6d68d598cd3c6
sriharibandi <shreharii@gmail.com>
2025-05-25 12:18:44 +0530

    practise5

--- backup-script.sh
+++ backup-script.sh
@@ -48,3 +48,16 @@ if [ $# -lt 2 ]
 then 
     USAGE 
 fi
+
+if [ ! -d $SOURCE_DIR ]
+then 
+    echo -e "$R $SOURCE_DIR doesn't exist. Please check $N"
+    exit 1
+fi
+
+if [ ! -d $DEST_DIR ]
+then 
+    echo -e "$R $DEST_DIR doesn't exist. Please check $N"
+    exit 1
+fi
+
