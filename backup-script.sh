da60b36f8b3f075db53ee82340bdfa1ccb4abbff
sriharibandi <shreharii@gmail.com>
2025-05-25 12:56:01 +0530

    practise5

--- backup-script.sh
+++ backup-script.sh
@@ -66,7 +66,7 @@ FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 if [ ! -z $FILES ]
 then
-    echo "Files found"
+    echo "Files to zip are: $FILES"
 else 
     echo -e "No log files found older than 14 days... $Y Skipping $N"
 fi
