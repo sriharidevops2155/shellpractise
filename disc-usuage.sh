c26187db016c77db023ff149ba466d6768beff84
sriharibandi <shreharii@gmail.com>
2025-05-29 00:10:11 +0530

    shell script practice

--- disc-usuage.sh
+++ disc-usuage.sh
@@ -9,9 +9,9 @@ do
     USUAGE=$(echo $line | awk '{print $6f}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7f}')
     echo "$PARTITION: $USUAGE"
-    if [ $USAGE -ge $DISK_THRESHOLD]
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then 
-        MSG="High Disk Usuage on $PARTITIO: $USUAGE "
+        MSG="High Disk Usuage on $PARTITIO: $USUAGE"
     fi
 done <<<$DISK_USUAGE
 
