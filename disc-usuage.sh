a0b89aea8d5135ce3733a8b2f7cc89349b1f0c49
sriharibandi <shreharii@gmail.com>
2025-05-29 00:09:04 +0530

    shell script practice

--- disc-usuage.sh
+++ disc-usuage.sh
@@ -2,12 +2,17 @@
 
 DISK_USUAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #in project it will be genarlly 75
+MSG=""
 
 while IFS= read line
 do
     USUAGE=$(echo $line | awk '{print $6f}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7f}')
-    echo "$PARTITION: $USUAGE" 
+    echo "$PARTITION: $USUAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD]
+    then 
+        MSG="High Disk Usuage on $PARTITIO: $USUAGE "
+    fi
 done <<<$DISK_USUAGE
 
 
