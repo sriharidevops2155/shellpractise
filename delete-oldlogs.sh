c918e57a876ff2ff3941b575ff792750119f0fec
sriharibandi <shreharii@gmail.com>
2025-06-02 15:26:17 +0530

    ansible

--- delete-oldlogs.sh
+++ delete-oldlogs.sh
@@ -9,7 +9,9 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SOURCE_DIR=/home/ec2-user/app-logs
+USERID=$(id -u)
 
+mkdir -p $LOGS_FOLDER
 
 #check the user have root privilages or not ? 
 if [ $USERID -ne 0 ]
@@ -31,9 +33,6 @@ VALIDATE()
         exit 1
     fi
 }
-
-
-mkdir -p $LOGS_FOLDER
 
 echo "Script started executing at $(date)" | tee -a $LOG_FILE 
 
